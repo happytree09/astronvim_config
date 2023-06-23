5bc4694e1ba5f34149fe3ddcdee350f6bcff34d7
masayahingpuno <rudyquismorio@gmail.com>
2023-06-23 18:01:37 +0800

    keybindings

--- mappings.lua
+++ mappings.lua
@@ -1,12 +1,8 @@
 -- TODO: Add mappings for the following
 -- 1. Between buffers
--- Mapping data with "desc" stored directly by vim.keymap.set().
---
--- Please use this mappings table to set keyboard mapping since this is the
--- lower level configuration and more robust one. (which-key will
--- automatically pick-up stored data by this setting.)
+
 return {
-  -- first key is the mode
+  -- Normal mode
   n = {
     -- second key is the lefthand side of the map
     -- mappings seen under group name "Buffer"
@@ -22,10 +18,28 @@ return {
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
+
+    -- navigate to buffer using shift + right arrow
+    ["<S-Right>"] = { ":bn<cr>", desc = "Next buffer" },
+
+    -- navigate to buffer using shift + left arrow
+    ["<S-Left>"] = { ":bp<cr>", desc = "Previous buffer" },
+
+    -- move to left splits using shift + up arrow
+    ["<S-Up>"] = { "<C-w>h", desc = "Left split" },
+
+    -- move to right splits using shift + down arrow
+    ["<S-Down>"] = { "<C-w>l", desc = "Right split" },
+
     -- quick save
     ["<C-s>"] = { ":w!<cr>", desc = "Save File" }, -- change description but the same command
+
     -- undo control z
     ["<C-z>"] = { ":undo<cr>", desc = "Undo" },
+
+    -- paste yank register
+    ["<C-v>"] = { '"*p', desc = "Paste" },
+
     -- go to the end of the line control right arrow
     ["<C-Right>"] = { "$", desc = "End of line" },
 
@@ -37,9 +51,46 @@ return {
 
     -- go to the top of the file
     ["<C-Up>"] = { ":0<cr>", desc = "Top of file" },
+
+    -- select all in the file using control a
+    ["<C-a>"] = { "ggVG", desc = "Select all" },
+
+    -- go the insert mode space space
+    ["<space><space>"] = { "i", desc = "Edit mode" },
   },
+
+  -- Edit mode sample
+  i = {
+    -- change to from insert mode to normal mode ctrl - q
+    ["<C-q>"] = { "<esc>", desc = "Normal mode" },
+
+    -- save using control s
+    ["<C-s>"] = { "<esc>:w!<cr>", desc = "Save File" },
+
+    -- undo control z
+    ["<C-z>"] = { "<esc>:undo<cr>", desc = "Undo" },
+  },
+  -- Visual mode
+  v = {
+    -- change to from visual mode to normal mode ctrl - q
+    ["<C-q>"] = { "<esc>", desc = "Normal mode" },
+
+    -- copy using control c
+    ["<C-c>"] = { '"*y', desc = "Copy" },
+
+    -- paste using control v
+    ["<C-v>"] = { '"*p', desc = "Paste" },
+
+    -- cut using control x
+    ["<C-x>"] = { '"*d', desc = "Cut" },
+
+    -- select all line using control l
+    ["<C-l>"] = { "V", desc = "Select all line" },
+  },
+
+  -- Terminal mode
   t = {
     -- setting a mapping to false will disable it
-    -- ["<esc>"] = false,
+    ["<esc>"] = false,
   },
 }
